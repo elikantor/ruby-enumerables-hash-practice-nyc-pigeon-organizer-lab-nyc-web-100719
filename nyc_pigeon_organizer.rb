63a40f59a1e1710bc24c95f5d56fa71cf60f8d94
Eli Kantor <eli.kantor@outlook.com>
2019-10-02 20:02:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,7 +12,9 @@ def nyc_pigeon_organizer(data)
         end
         
         if !new_hash[name][key]
-          new_hash[name][key] = 
+          new_hash[name][key] = []
+        end
+        
          binding.pry      
       }
   
