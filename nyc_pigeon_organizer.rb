9d9bbb0ad4a5f2bf00601dd71d69cc6112e8ff30
Eli Kantor <eli.kantor@outlook.com>
2019-10-02 20:08:44 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  
+  data.each { |key, value|
+    value.each { |subkey, subvalue|
+         
+      subvalue.each { |name|
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+        
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+        
+        new_hash[name][key] << subkey.to_s
+    
+      }
+  
+          
+    }
+  }
+
+new_hash
 end
