6e6db02263f4b8a9c46974df0f5dcdc5b4643048
Eli Kantor <eli.kantor@outlook.com>
2019-10-02 18:37:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,8 +5,9 @@ def nyc_pigeon_organizer(data)
   
   data.map { |color, color_key|
     color_key.map { |ele, name|
+      name = name.join
       if new_hash.any? != name
-        new_hash << name
+        new_hash = {name => color}
       end
     }
   }
